3f4853e1509dbf615c291cc6f66c168da631c637
Arimil <renari@arimil.com>
2024-12-02 12:51:58 -0500

    intial commit

---
+++ 1/Program.cs
@@ -0,0 +1,60 @@
+﻿const string input = "input.txt";
+
+if (!File.Exists(input))
+{
+    Console.WriteLine("No input file found, aborting!");
+    return;
+}
+
+using var sr = File.OpenText(input);
+List<uint> leftSide = [];
+List<uint> rightSide = [];
+Dictionary<uint, uint> rightSideCounts = [];
+while (sr.ReadLine() is { } line)
+{
+    var split = line.Split("   ");
+    if (split.Length != 2)
+    {
+        Console.WriteLine($"Invalid split length ({split.Length}) in input, aborting!");
+        return;
+    }
+
+    var leftNum = uint.Parse(split[0]);
+    var rightNum = uint.Parse(split[1]);
+
+    leftSide.Add(leftNum);
+    rightSide.Add(rightNum);
+    
+    if (rightSideCounts.TryGetValue(rightNum, out var value))
+    {
+        rightSideCounts[rightNum] = ++value;
+    }
+    else
+    {
+        rightSideCounts.Add(rightNum, 1);
+    }
+}
+    
+leftSide.Sort();
+rightSide.Sort();
+
+uint distance = 0;
+uint similarity = 0;
+for (var i = 0; i < leftSide.Count; i++)
+{
+    if (leftSide[i] > rightSide[i])
+    {
+        distance += leftSide[i] - rightSide[i];
+    }
+    else
+    {
+        distance += rightSide[i] - leftSide[i];
+    }
+    if (rightSideCounts.TryGetValue(leftSide[i], out var value))
+    {
+        similarity += leftSide[i] * value;
+    }
+}
+
+Console.WriteLine($"Distance: {distance}");
+Console.WriteLine($"Similarity: {similarity}");
---
+++ 2/Program.cs
@@ -0,0 +1,69 @@
+﻿const string input = "input.txt";
+
+if (!File.Exists(input))
+{
+    Console.WriteLine("No input file found, aborting!");
+    return;
+}
+
+using var sr = File.OpenText(input);
+var safeCount = 0;
+var safeCountWithDampener = 0;
+while (sr.ReadLine() is { } line)
+{
+    var numbers = line.Split(' ').Select(int.Parse).ToArray();
+    var descending = numbers[0] > numbers[1];
+    if (CheckSafety(numbers))
+    {
+        safeCount++;
+    }
+    else
+    {
+        // see if we can fix it with the dampener
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            // create a copy of this reading with a single entry removed
+            var dampenedNumbers = numbers.ToList();
+            dampenedNumbers.RemoveAt(i);
+            // check if this pattern is now safe
+            if (CheckSafety(dampenedNumbers.ToArray()))
+            {
+                safeCountWithDampener++;
+                break;
+            }
+        }
+    }
+    
+}
+
+Console.WriteLine($"Safe Count: {safeCount}");
+Console.WriteLine($"Safe Count With Dampener: {safeCount + safeCountWithDampener}");
+return;
+
+bool CheckSafety(int[] numbers)
+{
+    // a reading with a single entry can't break any of the rules
+    if (numbers.Length == 1) return true;
+    var descending = numbers[0] > numbers[1];
+
+    for (var i = 1; i < numbers.Length; i++)
+    {
+        // check for unsafe variance between steps
+        var difference = Math.Abs(numbers[i] - numbers[i - 1]);
+        if (difference is < 1 or > 3)
+        {
+            return false;
+        }
+
+        switch (descending)
+        {
+            // descending pattern but found ascending entry
+            case true when numbers[i] > numbers[i - 1]:
+            // ascending pattern but found descending entry
+            case false when numbers[i - 1] > numbers[i]:
+                return false;
+        }
+    }
+
+    return true;
+}
